df762884b21e8f31de97fdc39b3c2b2ca728eb49
OPDf10 <opdfibrahim10@gmail.com>
2023-09-14 18:34:04 -0700

    test

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,28 +1,15 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * mul - Multiplies two integers and returns the result.
- * @a: The first integer.
- * @b: The second integer.
+ * mul - Multiplies two integers.
+ * @a: The first integer to multiply.
+ * @b: The second integer to multiply.
  *
- * Return: The product of a and b.
+ * Return: The result of multiplying a and b.
  */
+
 int mul(int a, int b)
 {
-int product = a * b;
-return (product);
-}
-
-/**
- * main - Tests the mul function.
- *
- * Return: Always 0 (success).
- */
-
-int main(void)
-{
-printf("%d\n", mul(98, 1024));
-printf("%d\n", mul(-402, 4096));
-return (0);
+int result = a * b;
+return (result);
 }
