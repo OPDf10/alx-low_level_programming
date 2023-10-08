ea7de7ffc6e1203f3f097e940b92d87b4fb9436a
OPDf10 <opdfibrahim10@gmail.com>
2023-10-07 19:40:18 -0700

    test

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isdigit - Determines if a character is a digit.
+ * @c: Character to check.
+ *
+ * Return: 1 if character is a digit, 0 if not.
+ */
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
